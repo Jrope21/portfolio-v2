958e1d7ddcbb9542ccbfaacaeb922be0f7b76ecf
Jrope21 <joshbball8@gmail.com>
2019-08-26 07:47:19 -0500

    saving stuffz

--- portfolio-svelte-sap/src/components/ContactSection.svelte
+++ portfolio-svelte-sap/src/components/ContactSection.svelte
@@ -3,6 +3,13 @@
 </script>
 
 <style>
+    h2, p {
+        text-align: center;
+    }
+    div{
+        color: gray;
+        width: 60%;
+    }
     form{
         display: flex;
         flex-direction: column;
@@ -10,20 +17,38 @@
     label{
         display: flex;
         flex-direction: column;
+        margin: 5rem 0px;
     }
-    input{
-        border: 1px solid black;
+    span{
+        font-size: 10rem;
+        margin-bottom: 5rem;
+    }
+    input, textarea{
+        border: 1px solid gray;
+        border-radius: 5px;
+        font-size: 10rem;
+        padding: 5rem;
+    }
+    h2{
+        font-size: 20rem;
+    }
+    p{
+        font-size: 10rem;
+        color: lightgray;
     }
 </style>
-<h2>Get In Touch</h2>
-<form action="mailto:joshua.micah.roper@gmail.com" method="post">
-    <label> <span>Name</span>
-        <input type="text">
-    </label>
-    <label> <span>Email</span>
-        <input type="text">
-    </label>
-    <label> <span>Message</span>
-        <input type="textarea">
-    </label>
-</form>
+<div>
+    <h2>Get In Touch</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, dignissimos.</p>
+    <form action="mailto:joshua.micah.roper@gmail.com" method="post">
+        <label> <span>Name</span>
+            <input type="text">
+        </label>
+        <label> <span>Email</span>
+            <input type="text">
+        </label>
+        <label> <span>Message</span>
+            <textarea rows="6" type="textarea"> </textarea>
+        </label>
+    </form>
+</div>
