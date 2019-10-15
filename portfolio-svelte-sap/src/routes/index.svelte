f5d53ef0b09b32e2e0b2abeb55faac141b5e8321
Jrope21 <joshbball8@gmail.com>
2019-10-14 19:06:02 -0500

    testing variations

--- portfolio-svelte-sap/src/routes/index.svelte
+++ portfolio-svelte-sap/src/routes/index.svelte
@@ -5,53 +5,80 @@
 	import { onMount } from 'svelte';
     import HeroSection from '../components/HeroSection.svelte';
     import SingleProjectSection from '../components/SingleProjectSection.svelte';
-	import MultiProjectSection from '../components/MultiProjectSection.svelte';
+    import MultiProjectSection from '../components/MultiProjectSection.svelte';
+    import ProjectCardTextSection from '../components/ProjectCardTextSection.svelte';
     export let notHome = false;
 
 	let heroSection;
-	
-	// onMount(()=>{
-	// 	//ios fix
-	// 	let windowHeight = window.innerHeight;
-	// 	heroSection.style.height = `${windowHeight}px`
-	// })
+    
+    
+
+    // let x = .25;
+    let cards = [];
+
+    // onMount(()=>{
+    //     for(let i = 0; i < cards.length; i++){
+    //         let fadeInOrder = () => {
+                
+    //             cards[i].style.animation = `${'1'}s ease-in ${x}s 1 fadeInLeft forwards`;
+    //             x += .35;
+
+    //         }
+    //         fadeInOrder();
+    //     }
+    // })
+    
+
+    let portfolioCards = [
+        {
+            url: 'portfolio/university-park',
+            imgSrc: 'images/university-park-cta.jpeg',
+            projectName: 'University Park'
+        },
+        {
+            url: 'portfolio/creative-revolt',
+            imgSrc: 'images/bub.jpeg',
+            projectName: 'Creative Revolt'
+        },
+        {
+            url: 'portfolio/webuyuglyhouses',
+            imgSrc: 'images/WBUH.jpeg',
+            projectName: 'HomeVestors'
+        },
+        {
+            url: 'portfolio/anbtx',
+            imgSrc: 'images/university-park-cta.jpeg',
+            projectName: 'ANBTX'
+        },
+        {
+            url: 'portfolio/advancial',
+            imgSrc: 'images/university-park-cta.jpeg',
+            projectName: 'Advancial Credit Union'
+        },
+        {
+            url: 'portfolio/service-experts',
+            imgSrc: 'images/university-park-cta.jpeg',
+            projectName: 'Service Experts'
+        },
+    ]
 </script>
 
 <style>
 		section.one {
         width: 90vw;
-        height: 100vh;
+        height: 50vh;
+        min-height: 600px;
         margin: 0rem auto;
     }
-    /* section.one::before {
-        content: '';
-        background: rgba(34, 34, 34, 0.022);
-        position: absolute;
-        top: 0;
-        width: 100vw;
-        min-height: 100vh;
-      
-    }
-    section.one::after {
-        content: '';
-        background: rgba(34, 34, 34, 0.082);
-        position: absolute;
-        top: 0;
-        width: 100vw;
-        min-height: 100vh;
-      
-    } */
     section.two {
         width: 65vw;
         min-height: 820px;
         height: 80vh;
-        /* top: -40px; */
         position: relative;
         margin: auto;
     }
     section.two::before {
         content: '';
-        /* background: rgba(34, 34, 34, 0.022); */
         position: absolute;
         top: 0;
         width: 100vw;
@@ -99,6 +126,25 @@
         height: 100%;
       
     } 
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 10% 0 10% 0;
+        position: relative;
+    }
+    
+    @media screen and (min-width: 64em){
+        .container {
+            flex-direction: row;
+        }
+    }
+    div.projects-container {
+        width: 100%;
+        display:flex;
+        align-items: center;
+        flex-direction: column;
+    }
 </style>
 
 <svelte:head>
@@ -109,12 +155,24 @@
 	<section bind:this={heroSection} class="one center-all">
 		<HeroSection />
 	</section>
-    
-	<section class="two center-all">
+    <div class="container">
+        <div class="projects-container">
+            {#each portfolioCards as card, index}
+                <ProjectCardTextSection 
+                    bind:this={cards[index]}
+                    {index} 
+                    imgSrc={card.imgSrc} 
+                    url={card.url} 
+                    projectName={card.projectName}
+                />
+            {/each}
+        </div>
+    </div>
+	<!-- <section class="two center-all">
 		<SingleProjectSection />
 	</section>
     
 	<section class="three center-all">
 		<MultiProjectSection />
-	</section>
+	</section> -->
 </div>
