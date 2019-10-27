baecd0e766064fe9160b039cc8822ca7129f596a
Jrope21 <joshbball8@gmail.com>
2019-10-27 01:02:54 -0500

    mobile nav work

--- portfolio-svelte-sap/src/components/navigation/Navigation.svelte
+++ portfolio-svelte-sap/src/components/navigation/Navigation.svelte
@@ -1,23 +1,46 @@
 <script>
 import Hamburger from './Hamburger.svelte';
+
+let close;
+
 </script>
 
 <style>
 
 header {
     border-bottom: 1px solid #d6d6d6;
+    position: fixed;
+    width: 100%;
+    top: 0;
 }
 
 nav {
     display: flex;
-    /* width: 100%; */
     justify-content: space-between;
     align-items: center;
     padding: 20rem 20rem;
 }
 
 ul.navigation {
-    display: none;
+    box-sizing: border-box;
+    background: white;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    position: fixed;
+    z-index: 2;
+    width: 250px;
+    right: 0;
+    transform: translateX(100%);
+    bottom: 0;
+    height: 100%;
+    padding: 0rem 20rem;
+    transition: transform 1s cubic-bezier(0.85, 0.08, 0.08, 0.99)
+}
+
+#toggle:checked ~ ul.navigation{
+    display: flex;
+    transform: translateX(0);
 }
 
 @media screen and (min-width: 75em) {
@@ -26,7 +49,47 @@ ul.navigation {
     }
 }
 
+li{
+    text-align: right;
+    padding: 5rem 0rem;
+}
+
+.close-container {
+    position: absolute;
+    top: 50rem;
+    right: 50rem;
+}
+
+.close {
+    /* display: flex; */
+    width:32px;
+    height:32px;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+}
+
+.close::before {
+  content: '';
+  display: block;
+  position: absolute;
+  border-bottom: 2px solid black;
+  width: 32px;
+  transform: rotate(45deg);
+}
+
+.close::after {
+  content: '';
+  display: block;
+  position: absolute;
+  border-bottom: 2px solid black;
+  width: 32px;
+  transform: rotate(-45deg);
+}
+
 a {
+    display: block;
+    padding: 5rem 0rem;
     text-transform: uppercase;
 }
 
@@ -41,6 +104,7 @@ a {
         </div>
         <Hamburger />
         <ul class="navigation">
+            <li class="close-container" bind:this={close} ><span class="close"></span></li>
             <li><a href="">Home</a></li>
             <li><a href="">About</a></li>
             <li><a href="">Contact</a></li>
