0799ca926046005c0a530b9bd051a59fa9284dde
Jrope21 <joshbball8@gmail.com>
2019-12-01 23:13:13 -0600

    setting up contact modal

--- portfolio-svelte-sap/src/routes/index.svelte
+++ portfolio-svelte-sap/src/routes/index.svelte
@@ -2,32 +2,15 @@
 import Hero from '../components/hero/Hero.svelte';
 import Projects from '../components/projects/Projects.svelte'
 
-    // import { onMount } from 'svelte';  
-
-
-
-    // let x = .0;
-    // let sections = [];
-
-    // onMount(()=>{
-    //     for(let i = 0; i < sections.length; i++){
-    //         let fadeInOrder = () => {
-                
-    //             sections[i].style.animation = `${'.7'}s ease-in ${x}s 1 fadeIn forwards`;
-    //             x += .35;
-
-    //         }
-    //         fadeInOrder();
-    //     }
-    // })
 </script>
 
 <style>
+
     .container {
         position: relative;
         margin-top: 90px;
-        /* opacity: 0; */
     }
+
 </style>
 
 <div class="container">
