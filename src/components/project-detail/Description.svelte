e9a178fb14054c118f1f4e6aa674fe84268d362d
jrope21 <joshbball8@gmail.com>
2019-12-14 22:13:52 -0600

    commiting potential project-detail design -- preping to start work on 2 other variations -- Need to clean up code when done

--- src/components/project-detail/Description.svelte
+++ src/components/project-detail/Description.svelte
@@ -1,16 +1,72 @@
 <script>
+import TextAnimation from '../helper-components/TextAnimation.svelte';
 
 </script>
 
 <style>
-.headline {
-    font-size: 40px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    line-height: 45px;
-    font-weight: 700;
-    max-width: 700px;
-    color: #3B3B3B;
+  a{
+        display: flex;
+        align-items: center;
+        font-weight: 300;
+        text-transform: uppercase;
+        font-size: 12rem;
+        width: fit-content;
+        margin-top: 10rem;
+        color: #989898;
+        font-weight: 300;
+        font-style: italic;
+    }
+    h2 {
+        position: relative;
+        display: flex;
+        align-items: center;
+        margin: 15rem 0rem 5rem 0rem;
+        font-size: 13rem;
+        font-weight: 900;
+        text-transform: capitalize;
+        transition: all .2s ease-in-out;
+        color: #3B3B3B;
+        z-index: 2;
+    }
+
+    p{
+        position: relative;
+        line-height: 16px;
+        font-weight: 300;
+        font-size: 13rem;
+        z-index: 1;
+        color: #58595b;
+        text-indent: 10px;
+    }
+
+    @media screen and (min-width: 40em) {
+        h2 {
+            margin-top: 0rem;
+        }
+    }
+
+@media screen and (min-width: 64em){
+    h2 {
+        font-size: 23rem;
+    }
+    p{
+        font-size: 18rem;
+        line-height: 21px;
+    }
+    a{
+        font-size: 14rem;
+    }
+}
+.text-container {
+    position: sticky;
+    top: 130px;
 }
 </style>
 
-<p class="headline">Lorem Ipsum wassup dis be me son dawg crazy does dis look good</p>
+<div class="text-container">
+    <h2>Project Details</h2>
+    <p>I was tasked with being the <strong>sole developer</strong> on a <strong>complete Front-End redesign</strong>. Keeping their current users in mind, the goal was to make the website feel more modern, and offer a better user experience when navigating to each individual page. Across the entire project I implemented several dynamically generated content pages / sliders, <strong>form verification</strong>, and several <strong>third party integrations</strong>.</p>
+    <a target="blank" href="">
+        <TextAnimation text={`View Website`} />
+    </a>
+</div>
--- src/components/project-detail/ImageGrid.svelte
+++ src/components/project-detail/ImageGrid.svelte
@@ -1,8 +1,16 @@
 <script>
-import Image from '../projects/Image.svelte';
+import Image from './Image.svelte';
+import TopBar from './TopBar.svelte';
+import Description from './Description.svelte';
 </script>
 
 <style>
+@media screen and (min-width: 40em){
+    .inner-container {
+        width: 90%;
+        margin: 0 auto;
+    }
+}
 .image-grid {
     display: flex;
     flex-wrap: wrap;
@@ -13,10 +21,16 @@ import Image from '../projects/Image.svelte';
 
 @media screen and (min-width: 40em){
     .image-grid {
+        box-sizing: border-box;
         flex-direction: row;
         align-items: unset;
-        width: 90%;
+        width: 50%;
+        /* padding-left: 5%; */
         margin: 0 auto;
+    }
+    .project-description {
+        padding-top: 8%;
+        width: 50%;
     }
 }
 
@@ -44,29 +58,23 @@ import Image from '../projects/Image.svelte';
 }
 </style>
 
-<div class="image-grid">
-<div class="grid-element-container full-width"> <!-- this should be a cool designer image (logo shiz) -->
-        <Image width={'full-width'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} />
+<div class="inner-container">
+    <TopBar />
+    <div class="grid-x">
+        <div class="project-description">
+            <Description />
+        </div>
+        
+        <div class="image-grid">
+            <div class="grid-element-container full-width"> <!-- this should be a cool designer image (logo shiz) -->
+                <Image width={'full-width'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} />
+            </div>
+            <div class="grid-element-container full-width"> <!-- this should be a cool designer image (logo shiz) -->
+                <Image width={'full-width'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} />
+            </div>
+            <div class="grid-element-container full-width"> <!-- this should be a cool designer image (logo shiz) -->
+                <Image width={'full-width'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} />
+            </div>
+        </div>
     </div>
-    <div class="grid-element-container grid-60"> <!-- TODO fix image sizing -- should stay in container -->
-        <Image width={'large'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} /> <!-- this should be a tablet view  -- maybe wrap in a curved white border (mimic tablet view) -->
-    </div>
-    <div class="grid-element-container grid-40"> <!-- TODO fix image sizing -- should stay in container -->
-        <Image width={'large'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} /> <!-- this should be a mobile view -- maybe wrap in a curved white border (mimic mobile view)-->
-    </div>
-    <!-- <div class="grid-element-container">
-        <Image width={'large'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} />
-    </div>
-    <div class="grid-element-container">
-        <Image width={'large'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} />
-    </div> -->
-    <div class="grid-element-container full-width">
-        <Image width={'full-width'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} /> <!-- this should be a desktop view -- maybe make this one taller then first one??? (max-height 550px?)-->
-    </div>
-    <!-- <div class="grid-element-container full-width">
-        <Image width={'full-width'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} />
-    </div>
-    <div class="grid-element-container full-width">
-        <Image width={'full-width'} imgSrc={'images/Jorden-Background-Gray.jpg'} url={''} alt={''} />
-    </div> -->
 </div>
