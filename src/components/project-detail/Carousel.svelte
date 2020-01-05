915d619bc5f643838085162b6d35fa29a2a6ebcb
Jrope21 <joshbball8@gmail.com>
2020-01-05 17:39:24 -0600

    adjusting carousel look -- adding arrows

--- src/components/project-detail/Carousel.svelte
+++ src/components/project-detail/Carousel.svelte
@@ -4,19 +4,29 @@ import { fade, fly } from 'svelte/transition'
 let STATE = {
     images: [
         {
-            src: 'images/halcyon-5.jpg',
+            src: 'images/creative-revolt/home.png',
             visible: true,
-            key: 'desktop',
+            key: 'home',
         },
         {
-            src: 'images/uptexas-thumb.jpg',
+            src: 'images/creative-revolt/home-ctas.png',
             visible: false,
-            key: 'tablet',
+            key: 'home-ctas',
         },
         {
-            src: 'images/Jorden-Background-Gray.jpg',
+            src: 'images/creative-revolt/about.png',
             visible: false,
-            key: 'mobile',
+            key: 'about',
+        },
+        {
+            src: 'images/creative-revolt/about-cta.png',
+            visible: false,
+            key: 'about-ctas',
+        },
+        {
+            src: 'images/creative-revolt/writing-class.png',
+            visible: false,
+            key: 'writing-class',
         },
     ]
 }
@@ -43,6 +53,14 @@ function switchSlides(key) {
     display: flex;
     align-items: flex-end;
     margin: 40rem 0;
+    width: 100%;
+    min-height: 250px;
+}
+
+@media (min-width: 40em) {
+    .carousel-container {
+        width: auto;
+    }
 }
 
 @media screen and (min-width: 40em){
@@ -53,10 +71,11 @@ function switchSlides(key) {
         display: flex;
         align-items: center;
         justify-content: flex-end;
-        width: 237px;
+        width: 300px;
         border: 3px solid black;
         border-right: none;
-        height: 411px;
+        height: 380px;
+        opacity: 0;
         background: transparent;
     }
 
@@ -67,7 +86,7 @@ function switchSlides(key) {
         background-position-x: 1%;
         border-left: 3px solid black;
         height: 9%;
-        width: 150px;
+        width: 120px;
         right: -5px;
         bottom: -3px;
     }
@@ -82,7 +101,17 @@ function switchSlides(key) {
     }
 }
 
-@media screen and (min-width: 64em) {
+@media (min-width: 64em) {
+    .box {
+        width: 400px;
+        height: 516px;
+    }
+    .box::after {
+        width: 150px;
+    }
+}
+
+@media (min-width: 1366px) {
     .box {
         width: 500px;
         height: 616px;
@@ -90,45 +119,142 @@ function switchSlides(key) {
 }
 
 .title {
+    display: none;
     font-size: 40px;
+    position: absolute;
     color: #58595b;
     transform: rotate(-90deg);
     margin-right: -15px;
+    top: 50%;
+    left: -20%;
 }
+
+@media (min-width: 40em) {
+    .title {
+        display: inline;
+        left: -25%
+    }
+}
+
+@media (min-width: 64em) {
+    .title {
+        font-size: 55px;
+    }
+}
+
+@media (min-width: 1366px) {
+    .title {
+        font-size: 67px;
+    }
+}
+
 
 .slide {
     position: absolute;
-    width: 88%;
-    height: 82%;
-    right: 0;
-    top: 50%;
-    transform: translateY(-50%);
+    left: 50%;
+    transform: translateX(-50%);
+    width: 450px;
+    max-width: 85vw;
+    height: 250px;
     box-shadow: 3px 3px 3px lightgrey;
+    margin: 0 auto;
 }
 
-img {
+@media (min-width: 40em){
+    .slide {
+        left: auto;
+        position: absolute;
+        width: 88%;
+        height: 82%;
+        right: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        margin: 0;
+    }
+}
+
+.slide img {
     width: 100%;
     height: 100%;
-    object-fit: cover;  
+    object-fit: cover;
+    object-position: top;  
 }
 
 div.circles-container {
+    width: 450px;
+    /* height: 250px; */
+    bottom: -30px;
+    margin: 0 auto;
+    max-width: 85vw;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
     display: flex;
-    margin-left: -70px;
-    margin-bottom: 20px;
+    justify-content: flex-start;
+    align-items: flex-start;
+    /* flex-direction: column; */
+    /* background: rgba(0,0,0,0.3); */
+}
+
+@media (min-width: 40em) {
+    div.circles-container {
+        height: auto;
+        width: auto;
+        /* position: static;
+        margin-left: -10px;
+        margin-bottom: 10px; */
+        transform: none;
+        left: 12%;
+        bottom: 0;
+        /* top: 9%; */
+        /* justify-content: flex-start; */
+        /* flex-direction: column; */
+        
+    }
+}
+
+@media (min-width: 64em) {
+
+    div.circles-container {
+        /* margin-left: -30px;
+        margin-bottom: 20px; */
+        
+    }
 }
 
 span.circle {
     position: relative;
     display: block;
-    width: 15px;
-    height: 15px;
-    border: 1px solid #3B3B3B;
+    width: 12px;
+    height: 12px;
+    border: 2px solid #3B3B3B;
     border-radius: 50%;
-    margin-right: 25px;
+    margin: 0px 20px 0 0px;
     overflow: hidden;
     cursor: pointer;
-    /* transition: opacity .3s ease; */
+}
+
+@media (min-width: 40em) {
+    span.circle {
+        margin: 0;
+        margin-right: 15px;
+        /* border: 1px solid #3B3B3B; */
+    }
+}
+
+@media (min-width: 64em) {
+    span.circle {
+        width: 15px;
+        height: 15px;
+    }
+}
+
+@media (min-width: 1366px) {
+    span.circle {
+        width: 18px;
+        height: 18px;
+        margin-right: 20px;
+    }
 }
 
 span.circle::before {
@@ -147,17 +273,104 @@ span.circle::before {
 span.circle.active::before {
     opacity: 1;
 }
+
+button.next, button.back {
+    position: absolute;
+    width: 150px;
+    height: 100%;
+    
+    opacity: .3;
+    transition: all .3s ease-out;
+}
+
+button.next {
+    right: 0;
+    background: linear-gradient(to left,rgba(0,0,0,.65) 0,rgba(0,0,0,0) 100%) rgba(0,0,0,0);
+}
+
+button.back {
+    left: 0;
+    background: linear-gradient(to right,rgba(0,0,0,.65) 0,rgba(0,0,0,0) 100%) rgba(0,0,0,0)
+}
+
+button.next:hover, button.back:hover {
+    opacity: 1;
+}
+
+.arrow-left, .arrow-right {
+    display: block;
+    position: absolute;
+    height: 20px;
+    width: 20px;
+    top: calc(50% - 10px);
+    -webkit-transition: -webkit-transform .2s ease-out;
+    -webkit-transition-delay: .2s;
+    transition: transform .2s ease-out .2s;
+}
+
+.arrow-left {
+    left: 30px;
+}
+
+.arrow-right {
+    right: 30px;
+}
+
+.arrow-left::before, .arrow-right::before, .arrow-left::after, .arrow-right::after {
+    content: " ";
+    position: absolute;
+    right: 0;
+    height: 100%;
+    width: 2px;
+    background-color: white;
+    opacity: .7;
+    -webkit-transform-origin: 50% 100% 0;
+    transform-origin: 50% 100% 0;
+    -webkit-transition: -webkit-transform .15s ease-out;
+    transition: transform .15s ease-out;
+}
+
+.arrow-left::before {
+    top: -50%;
+    -webkit-transform: rotate(45deg);
+    transform: rotate(45deg);
+}
+
+.arrow-left::after {
+    top: calc(-50% + -1px);
+    -webkit-transform: rotate(135deg);
+    transform: rotate(135deg);
+}
+
+.arrow-right::before {
+    top: -50%;
+    -webkit-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+}
+
+.arrow-right::after {
+    top: calc(-50% + -1px);
+    -webkit-transform: rotate(-135deg);
+    transform: rotate(-135deg);
+}
 </style>
 
 <!-- notes to come back to -->
 <!-- TODO -  -->
 <div class="carousel-container">
-    <div class="box"><span class="title">CREATIVE REVOLT</span></div> <!-- TODO style text to appear nice on fade out-->
+<span class="title">CREATIVE REVOLT</span>
+    <div class="box"></div> <!-- <span class="title">CREATIVE REVOLT</span> TODO style text to appear nice on fade out-->
     <!-- TODO - make top of box longer then bottom -->
         {#each STATE.images as img}
             {#if img.visible}
-                <div transition:fly="{{ x: -80, duration: 1050 }}" class="slide">
+                <div transition:fly="{{ x: -40, duration: 550 }}" class="slide">
+                    <button class="back">
+                        <span class="arrow-left"></span>
+                    </button>
                     <img src="{img.src}" alt="wassup">
+                    <button class="next">
+                        <span class="arrow-right"></span>
+                    </button>
                 </div>
             {/if}
         {/each}
